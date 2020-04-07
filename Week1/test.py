02a043fc3f0c1cc27733baa95d952c78f9f4737c
sahasra-01 <sahasraranjan@gmail.com>
2020-04-07 10:30:30 +0530

    few bugs

--- Week1/test.py
+++ Week1/test.py
@@ -1,41 +1,8 @@
-# m[roll] = marks
-# m[marks] = count
-# m[marks] = rank
-import csv
+a = [[0]*5]*5
 
-marks = {}
+for i in range(5):
+	for j in range(5):
+		print(i, j)
+		a[i][0] = i+j
 
-with open('test.csv', 'r') as file:
-	reader = csv.reader(file)
-	for row in reader:
-		marks[ row[0] ] = row[1]
-
-count = {}
-rank = {}
-
-r = 1
-l = 100000
-for m in sorted(marks.items(), key=lambda item:item[1] , reverse=True):
-	rank[m[1]] = r
-	print (m)
-
-	if(m[1] != l):
-		count[m[1]] = 1
-		if(l != 100000):
-			r += count[l]
-		else:
-			r += 1		
-	else:
-		count[m[1]] += 1
-
-
-
-	l = m[1]
-
-
-while True:
-	qr = input("Enter Roll Number: ")
-	if(qr == "stop"):
-		break;
-
-	print("Marks:", marks[qr], ", Rank:", rank[ marks[qr] ], ", Tied Between:", count[ marks[qr] ])
+print(a)
