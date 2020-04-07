1a4a3a63e436b7fdaf1b402db248d87c60e9898a
sahasra-01 <sahasraranjan@gmail.com>
2020-04-07 10:22:51 +0530

    Task 3.1 finalized

--- Week1/Dijkstra.py
+++ Week1/Dijkstra.py
@@ -3,14 +3,16 @@ maxint = 999999999
 
 class Graph():
 
+	# Constructor, Note: def of graph ( do not use [[0]*n]*n, this will throw error while assigning values)
+	# [[0]*n]*n  returns same reference to all the lists
 	def __init__(self, vertices):
 		self.V = vertices
-		self.graph = [[0]*vertices]*vertices
+		self.graph =[[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
 
-
+    # Return which index to select for next iteration
 	def minDistance(self, dist, sptSet):
-		min = dist[0]
-		min_index = 0
+		min = maxint
 
 		for v in range(self.V):
 			if (sptSet[v] == False and dist[v] < min):
@@ -20,31 +22,34 @@ class Graph():
 		return min_index
 
 
+	# Dijksta's Algorithm
 	def dijkstra(self, src):
 		dist = [maxint]*self.V
 		sptSet = [False]*self.V
 
 		dist[src] = 0
 
-		# print(self.graph)
-
+		# V-1 loop to assign min distance all the vertices from source
 		for count in range(self.V-1):
 			u = self.minDistance(dist, sptSet)
 
 			sptSet[u] = True
 
+			# If shorter than previously assigned path
 			for v in range(self.V):
 				if (sptSet[v] == False and self.graph[u][v]!=0 and dist[u] != maxint and dist[u] + self.graph[u][v] < dist[v]):
 					dist[v] = dist[u] + self.graph[u][v]
 
-		print (self.graph)
 		print (dist)
 
 
 n = int(input())
-g = Graph(n)
+g = Graph(n)		# Instantiation, object g of class Graph
 
-
+# Graph is represented as 2D array(list here)
+# g[i][j] = 0, means no edge
+# else: g[i][j] = w  means an egde from i to j of weight w (directed)
+# here since undirected, g[i][j] = g[j][i] = w
 for i in range(n):
 	edge = int(input())
 	for t in range(edge):
@@ -52,9 +57,6 @@ for i in range(n):
 		j = int(t1)
 		w = int(t2)
 		g.graph[i][j] += w
-		g.graph[j][i] += w
-
-print(g.graph)		
-
+		g.graph[j][i] += w	
 
 g.dijkstra(0)
